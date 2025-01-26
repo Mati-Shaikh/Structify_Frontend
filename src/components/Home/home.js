655dd7312e94db20cf64e6c99e8bfc20341e2a48
Afnan Naseem <afnannaseem837@gmail.com>
2025-01-26 19:43:37 +0500

    Test Commit

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -4,7 +4,7 @@ import { User, Settings, LogOut } from "lucide-react";
 import { motion } from "framer-motion";
 import { ChevronRight, Flame } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-
+//hello
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
